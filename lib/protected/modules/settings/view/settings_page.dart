888a80398ea8d0bc8fb3cc2ce9946cb18ba008cf
Conrad Hoang <tcmhoang@outlook.com>
2022-08-08 05:16:32 -0400

    feat: implement nested navigation

---
+++ lib/protected/modules/settings/view/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'settings_view.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SettingsView();
+}
